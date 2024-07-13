8ecda1e1f624a47906c274ea262e6eb0eddd7620
Jiwnasted <jiwjiw2606@gmail.com>
2024-07-13 21:09:06 +0700

    conv

--- scenes/DialogueController.gd
+++ scenes/DialogueController.gd
@@ -10,6 +10,25 @@ func _ready():
 	await get_tree().create_timer(0).timeout
 	panel.position.y = viewsize.size.y + panel.size.y
 	
+	var DialoguePath = ["res://assets/conversation/paco.json"]
+	var file = FileAccess.open(DialoguePath[0], FileAccess.READ)
+	var json_string = file.get_as_text()
+	
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	var data
+	
+	if error == OK:
+		data = json.data
+		print("OK " + DialoguePath[0])  # This will print the parsed JSON data
+	else:
+		data = "none"
+		print("Error parsing JSON: ", json.get_error_message())
+	
+	print(data.conversation[0])
+	file.close()
+	
+	
 func _process(delta):
 	pass
 
@@ -18,14 +37,14 @@ var togg = false
 func _on_test_1_button_down():
 	
 	var pos = viewsize.size.y - panel.size.y - 80
-	print(panel.size.y)
+	#print(panel.size.y)
 	if togg :
 		togg = false
 		pos = viewsize.size.y + panel.size.y
 	else :
 		togg = true
 		pos = viewsize.size.y - panel.size.y - 80
-	print(pos)
+	#print(pos)
 	var tween = create_tween()
 	
 	tween.tween_property(panel, 'position', Vector2(0, pos), .6).set_trans(Tween.TRANS_QUINT)
