f406f4a57be0f124feb112e58a6e0a06a6e53d7b
Poomyyyy <8pitipoomsaripan@gmail.com>
2024-07-16 21:18:05 +0700

    Npc test Update

--- Testnpcrandom/Npc/npc.gd
+++ Testnpcrandom/Npc/npc.gd
@@ -1,15 +1,31 @@
 extends CharacterBody2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+
+const GRAVITY = 200
+const MAX_SPEED = 200
+const JUMP_FORCE = 100
+
+@onready var body = $Body
+@onready var paw = $Paw
+@onready var skin = $Skin
+@onready var eye = $Eye
+@onready var outline = $Outline
+
+func _on_Body_frame_changed():
+	sync_frames(body.frame, body.flip_h)
+
+func sync_frames(frame, flip_h):
+	body.frame = frame
+	paw.frame = frame
+	skin.frame = frame
+	eye.frame = frame
+	outline.frame = frame
+	
+	body.flip_h = flip_h
+	paw.flip_h = flip_h
+	skin.flip_h = flip_h
+	eye.flip_h = flip_h
+	outline.flip_h = flip_h
+
+var npcbodycolors
+var npceyecolors
+func initialize(_)
--- Testnpcrandom/Npcspawn/npcspawner.gd
+++ Testnpcrandom/Npcspawn/npcspawner.gd
@@ -1,11 +1,24 @@
 extends Node2D
+var npc = preload("res://Testnpcrandom/Npc/npc.tscn")
 
+@onready var timer = $Timer
+@export var timespwn : int = 1 #seconds to wwait spawning another npc
 
-# Called when the node enters the scene tree for the first time.
+@export var npcbodycolors : PackedColorArray = [Color("ffb892"), Color("ec9a74"), Color("d77e5e"), Color("ac6758")]
+@export var npceyecolors : PackedColorArray = [Color("ffb892"), Color("ec9a74"), Color("d77e5e"), Color("ac6758")]
 func _ready():
-	pass # Replace with function body.
+	timer.start(timespwn)
 
+func spawn():
+	var instance = npc.instance()
+	var pos = Vector2(position.x + randf_range(0,10), position.y)
+	instance.initialize(pos, randitem(npcbodycolors), randitem(npceyecolors))
+	get_tree().current_scene.add_child(instance)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func randitem(array : Array):
+	randomize()
+	array.shuffle()
+	return array.front()
+
+func _on_Timer_timeout():
+	spawn()
